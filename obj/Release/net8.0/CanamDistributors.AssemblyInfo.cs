f1d745f4b59a76b97df60e66d8ef195efafc770e
SahilAttri740 <Sahil.attri@techrbm.com>
2024-10-19 11:33:07 +0530

    push to main

--- obj/Release/net8.0/CanamDistributors.AssemblyInfo.cs
+++ obj/Release/net8.0/CanamDistributors.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CanamDistributors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b8d9610cee6cbceb39264d01c2029a3ec4dced")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ad1a224900dd9ed85cbb427dadff386f5eeb2b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CanamDistributors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CanamDistributors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
